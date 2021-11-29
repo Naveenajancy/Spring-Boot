5aa42cd3c94fa70f9a222467155d619e03ad292b
nkb05 <kb.jancy90@gmail.com>
2021-11-29 12:21:49 -0500

    project setup

---
+++ h2demo/src/main/java/com/example/naveenah2demo/NaveenaH2DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.naveenah2demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaveenaH2DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NaveenaH2DemoApplication.class, args);
+    }
+
+}
